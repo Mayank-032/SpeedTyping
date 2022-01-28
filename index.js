efc24077eefb5e9e1073cba3fccf7f84699dda33
Mayank-032 <mayankmohan74@gmail.com>
2022-01-28 10:14:00 +0530

    proper timer and characters processing

--- index.js
+++ index.js
@@ -1,11 +1,66 @@
-let str = "If you're looking for random paragraphs";
+let str = "If you're looking for random paragraphs. We recommend reading this tutorial, in the sequence listed in the menu."+
+"If you have a large screen, the menu will always be present on the left. If you have a small screen, open the menu by clicking the top menu sign;";
 
-let readOnlyTextArea = document.getElementById("myReadOnlyTextArea");
-readOnlyTextArea.value = str;
+let readOnlyText = document.querySelector(".myReadOnlyTextArea");
+let writeOnlyText = document.querySelector(".myWriteOnlyTextArea");
 
-let writeOnlyTextArea = document.getElementById("myWriteOnlyTextArea");
+displayText();
+function displayText(){
+    readOnlyText.textContent = null;
+
+    str.split('').forEach(char => {
+        let charSpan = document.createElement('span');
+        charSpan.appendChild(document.createTextNode(char));
+        readOnlyText.appendChild(charSpan);
+    })
+}
+let readOnlyTextArray = readOnlyText.querySelectorAll("span");
+
+
+let index = 0;
+let char = readOnlyTextArray[index];
+char.classList.add("current_value_property");
+function processCurrentText(event) {
+    let typedChar = event.key;
+
+    if(typedChar === 'Backspace'){
+        let prevChar = readOnlyTextArray[index];
+        prevChar.classList.remove("current_value_property")
+        prevChar.classList.remove('current_value_property');
+        prevChar.classList.remove("myTextIncorrect");
+
+        index--;
+        let char = readOnlyTextArray[index];
+        char.classList.add("current_value_property");
+    }else if(typedChar === 'CapsLock' || typedChar === 'Shift'){
+        // console.log(",");
+    }else{
+        let char = readOnlyTextArray[index];
+
+        if(typedChar === char.innerText){
+            char.classList.add("myTextCorrect");
+            char.classList.remove("myTextIncorrect");
+            char.classList.remove("current_value_property");
+            index++;
+
+            char = readOnlyTextArray[index];
+            char.classList.add('current_value_property');
+        }else if(typedChar !== char.innerText){
+            char.classList.add("myTextIncorrect");
+            char.classList.remove("myTextCorrect");
+            char.classList.remove("current_value_property");
+            index++;
+
+            char = readOnlyTextArray[index];
+            char.classList.add('current_value_property');
+        }
+    }
+}
+
+
+
+
 let control_Buttons = document.getElementById("controlButtons");
-
 let timeLimit = document.getElementById("chooseTime").value;
 document.getElementById("chooseTime").onchange = function(){
     timeLimit = this.value;
@@ -38,17 +93,24 @@ function reset() {
 function timer() {
     if(timeLimit > 2){
         if(second >= timeLimit){
+            writeOnlyText.disabled = true;
             pause();
             reset();
+            i=0;
+            writeOnlyText.value = "";
+            writeOnlyText.disabled = false;
         }
     }else{
         if(minute >= timeLimit){
+            writeOnlyText.disabled = true;
             pause();
             reset();
+            writeOnlyText.value = "";
+            writeOnlyText.disabled = false;
         }
     }
 
-    if((millisecond += 10) == 1000){
+    if((millisecond += 20) == 1000){
         millisecond = 0;
         second++;
     }
